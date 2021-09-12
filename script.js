62ceee51edb064a7223beacc680133d54287f3f5
Ajay <ajaybains2004@gmail.com>
2021-09-12 12:28:31 +0530

    previous functionality added and made responsive

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const next = document.querySelector(".next");
 const previous = document.querySelector(".previous");
 const container = document.querySelector(".container");
 const title = document.querySelector(".music-title");
+
+// MUSIC DATA!
 const srcMusic = [
   { sound: "bensound-acousticbreeze.mp3", pic: "acoustic.jpg" },
   { sound: "bensound-betterdays.mp3", pic: "sunnyday.jpg" },
@@ -16,55 +18,65 @@ const srcMusic = [
   { sound: "bensound-goinghigher.mp3", pic: "higher.jpg" },
   { sound: "bensound-sunny.mp3", pic: "sunshine.jpg" },
 ];
-// const srcMusic = [
-//   "bensound-acousticbreeze.mp3",
-//   "bensound-betterdays.mp3",
-//   "bensound-clearday.mp3",
-//   "bensound-creativeminds.mp3",
-//   "bensound-evolution.mp3",
-//   "bensound-funnysong.mp3",
-//   "bensound-goinghigher.mp3",
-//   "bensound-sunny.mp3",
-// ];
 
+// Applying background to default song
 container.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)),url('../images/${srcMusic[0].pic}')`;
+
+// setting  dynamically inital src attribute to audio file
 const source = document.querySelector("source");
 const sourcePlace = source.setAttribute("src", `./music/${srcMusic[0].sound}`);
 title.innerText = `${srcMusic[0].sound}`;
+
+// Play Music function
 
 const playMusic = () => {
   playing = !playing;
   if (playing === true) {
     music.play();
-    // btn.innerText = "pause";
     btn.innerHTML = "<i class='fas fa-pause'>";
   } else {
     music.pause();
     btn.innerHTML = "<i class='fas fa-play'>";
   }
-  console.log(playing);
 };
-
+// Navigating through the playList
 let i = 0;
-const playNext = () => {
+
+// play the music  function(used in next and previous clicks)
+const musicPlay = () => {
   music.pause();
   music.currentTime = 0;
+  source.setAttribute("src", `./music/${srcMusic[i].sound}`);
+  container.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)),url('../images/${srcMusic[i].pic}')`;
+  title.innerText = `${srcMusic[i].sound}`;
+  music.load();
+  music.play();
+};
+
+// Play next song
+const playNext = () => {
   i++;
   if (i < srcMusic.length) {
-    source.setAttribute("src", `./music/${srcMusic[i].sound}`);
-    container.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)),url('../images/${srcMusic[i].pic}')`;
-    title.innerText = `${srcMusic[i].sound}`;
-    music.load();
-    music.play();
+    musicPlay();
   } else {
     i = 0;
 
-    music.play();
+    musicPlay();
   }
 };
+
+// Play previous song
 const playPrevious = () => {
-  console.log("clicked previous");
+  i--;
+  console.log(i);
+  if (i >= 0 && i < srcMusic.length) {
+    musicPlay();
+  } else {
+    i = srcMusic.length - 1;
+    musicPlay();
+  }
 };
+// event listener on button controls
 btn.addEventListener("click", playMusic);
 next.addEventListener("click", playNext);
 previous.addEventListener("click", playPrevious);
